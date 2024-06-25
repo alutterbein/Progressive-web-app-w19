cfdfc17b028dfbe820b1a365aaa8102097fcc1cf
alutterbein <alutterbein@gmail.com>
2024-06-24 18:05:56 -0700

    webpack config

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -18,13 +18,59 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'text editor',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest:'sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'text editor',
+        short_name: 'text',
+        description: 'just another text editor',
+        background_color: 'black',
+        theme_color: 'green', 
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.resolv.join("assets", "icons"),
+          },
+        ],
+      }),
 
+
+
+    ],
+// css & babel loader
     module: {
       rules: [
-        
+        {
+         test: /\.css$/i,
+         use: ['style-loader', 'css-loader'],
+
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
       ],
     },
-  };
-};
+          },
+        },
+        ],
+      },
+            };
+          };
